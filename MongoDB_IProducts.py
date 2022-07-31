d06499e8ecc52ff2dfbe1bc78f8a5a7fea4c7a79
sfbarragan <sfbarragan@espe.edu.ec>
2022-07-30 21:29:21 -0500

    se corrigio un error

--- MongoDB_IProducts.py
+++ MongoDB_IProducts.py
@@ -18,7 +18,7 @@ coleccion = db['Products']
 start = time.perf_counter()
 
 for num in range(1, cantidad):    
-    coleccion.insert_one({"ProductID": str(num) , "ProductName": "ProductName" + str(num), "SupplierID": "1", "CategoryID": "1", "QuantityPerUnit": 1, "UnitPrice": 2, "UnitsInStock": 23, "UnitsOnOrder": 1323 "ReorderLevel": 5, "Discontinued": 1})
+    coleccion.insert_one({"ProductID": str(num) , "ProductName": "ProductName" + str(num), "SupplierID": "1", "CategoryID": "1", "QuantityPerUnit": 1, "UnitPrice": 2, "UnitsInStock": 23, "UnitsOnOrder": 1323, "ReorderLevel": 5, "Discontinued": 1})
     
 end = time.perf_counter()
 
